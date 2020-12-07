19c66b27e1424d85efc0a3c9c1a20cc5d3bc851a
Lukasz <lukasz.d.zajac@gmail.com>
2020-12-07 10:46:20 +0100

    Initial Commit

---
+++ bulls2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bulls2
+//
+//  Created by Lukasz Zajac on 07/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bulls2/bulls2App.swift
@@ -0,0 +1,17 @@
+//
+//  bulls2App.swift
+//  bulls2
+//
+//  Created by Lukasz Zajac on 07/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct bulls2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
